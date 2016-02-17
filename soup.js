a457b8436e8b511d2416e4790007aba58e1a154d
Mireille Bonapart <mireilleb1202@gmail.com>
2016-02-17 13:19:44 +0100

    Change of media queries and add js event

--- soup.js
+++ soup.js
@@ -1,20 +1,7 @@
 
 $(document).ready(function() { 
 	/*slowly fade in and fade out header*/
-    $('#main-header').fadeOut(1000); $('#main-header').fadeIn(5000);
+    $('#headertitle').fadeOut(1000); $('#headertitle').fadeIn(5000);
     
 });
 
-var checkOpenStatus = function () {
-    var d = new Date();
-    var date = d.getHours();
-    var day = d.getDay();
-    if ((date > 9) && (date < 20) && (day != 0)) {
-        y = "<span style=\"color:#FFFFFF\">We zijn open en de soep staat klaar!</span>";
-    } else {
-        y = "<span style=\"color:#FFFFFF\">Sorry, we zijn gesloten.</span>";
-    }
-    document.getElementById("open-dicht").innerHTML = y;
-}
-
-setInterval(checkOpenStatus,100);
