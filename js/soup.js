d65e0c4e67bf19847a3942bdfeded822442b3210
Mireille Bonapart <mireilleb1202@gmail.com>
2016-03-08 17:28:29 +0100

    Change columns

--- js/soup.js
+++ js/soup.js
@@ -1,13 +1,7 @@
 
-$(document).ready(function() { 
-	/*slowly fade in and fade out header*/
-    $('#headertitle').fadeOut(1000); $('#headertitle').fadeIn(5000);
-    
-});
-
  $(function () {
             $(window).on("load resize", function () {
                 $(".fill-screen").css("height", window.innerHeight);
-            });
+     });
         
 });
