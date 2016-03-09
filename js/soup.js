600565cc0ed21a55ad829dc40a8ecba8508784d9
Mireille Bonapart <mireilleb1202@gmail.com>
2016-03-09 12:15:19 +0100

    Add scrollingand parallax effect

--- js/soup.js
+++ js/soup.js
@@ -1,7 +1,24 @@
 
- $(function () {
+$(function () {
             $(window).on("load resize", function () {
                 $(".fill-screen").css("height", window.innerHeight);
      });
-        
+
+  // add Bootstrap's scrollspy
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 200
+    });
+
+    // smooth scrolling
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 50
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    //parallax scrolling with stellar.js
+    $(window).stellar();
+
 });
