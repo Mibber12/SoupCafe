df4ac3d144e9780cbf8387780ed25c3bd8a663b6
Mireille Bonapart <mireilleb1202@gmail.com>
2016-03-28 12:37:42 +0200

    Add media icons and dropdownmenu

--- js/soup.js
+++ js/soup.js
@@ -1,24 +1,28 @@
 
-$(function () {
-            $(window).on("load resize", function () {
-                $(".fill-screen").css("height", window.innerHeight);
-     });
 
-  // add Bootstrap's scrollspy
+$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+    // add Bootstrap's scrollspy
     $('body').scrollspy({
         target: '.navbar',
-        offset: 200
+        offset: 160
     });
 
     // smooth scrolling
     $('nav a, .down-button a').bind('click', function () {
         $('html, body').stop().animate({
-            scrollTop: $($(this).attr('href')).offset().top - 50
-        }, 1000, 'easeInOutExpo');
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 
-    //parallax scrolling with stellar.js
-    $(window).stellar();
-
 });
+
+    //initialize WOW for element animation
+    new WOW().init();	
+
+
+
