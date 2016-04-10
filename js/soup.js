8baf40420f2814506db422553b265013bb2e67c5
Mireille Bonapart <mireilleb1202@gmail.com>
2016-04-10 17:34:25 +0200

    Add media query FB plugin and function navbar collapse

--- js/soup.js
+++ js/soup.js
@@ -1,6 +1,10 @@
- $(document).ready(function () {
-        $('.dropdown-toggle').dropdown();
-    });
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
+
 
 $(function () {
     $(window).on("load resize", function () {
